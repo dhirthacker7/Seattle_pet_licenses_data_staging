e93df5723e3b1caca126aca0a1caf2ebd9e5305d
Dhir Thacker <46825149+dhirthacker7@users.noreply.github.com>
2024-05-10 20:11:30 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+use seattle_pet_license;
+
+select count(*) as "Total_count_of_records" from stg_seattle_pet_licenses 
+
+select * from stg_seattle_pet_licenses
---
+++ SQLQuery2.sql
@@ -0,0 +1,3 @@
+use seattle_pet_license
+
+exec sp_help 'dbo.stg_seattle_pet_licenses' 
